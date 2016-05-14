61d7bfa9693eaa062a20ee1752fb7fd85c54dcb0
joetheory <joetheory@gmail.com>
2016-05-14 00:20:40 -0400

    SAME!

--- tayne.js
+++ tayne.js
@@ -31,6 +31,9 @@ client.Dispatcher.on("MESSAGE_CREATE", e => {
 	if(incoming_text == '!naptime')  {
 		e.message.channel.uploadFile("img/billynap.gif"); // File
 		found = true;
+	} else if(incoming_text == '!same')  {
+		e.message.channel.uploadFile("img/same.gif"); // File
+		found = true;
 	} else if(incoming_text == '!mark' || incoming_text == '!tank') { // Mark Nickerson, our hero
 		e.message.channel.uploadFile("img/tank.jpg"); // File
 		return;
