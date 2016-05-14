6e824a58b93fe7466b534bd4d43ca85eb462163c
cstabb <chris.stabb@gmail.com>
2016-05-14 18:30:22 -0400

    Made quotes consistent

--- tayne.js
+++ tayne.js
@@ -7,7 +7,7 @@ client.connect({ token: token });
 
 client.Dispatcher.on("GATEWAY_READY", e => {
 	console.log("New Event: GATEWAY_READY");
-	console.log('Hey, I\'m ' + client.User.username + ', your latest dancer. I can\'t wait to entertain you.');
+	console.log("Hey, I'm " + client.User.username + ", your latest dancer. I can't wait to entertain you.");
 });
 
 client.Dispatcher.on("MESSAGE_CREATE", e => {
@@ -16,9 +16,9 @@ client.Dispatcher.on("MESSAGE_CREATE", e => {
 
 	var incoming_text = e.message.content;
 
-	if(incoming_text.substring(0,2) == '!v') {
+	if(incoming_text.substring(0,2) == "!v") {
 		vgs = require("./vgs.json");
-		vgs_arr = eval("vgs." + incoming_text.substring(1).split(' ', 1) + "['text']");
+		vgs_arr = eval("vgs." + incoming_text.substring(1).split(" ", 1) + "['text']");
 		if(vgs_arr.length > 1) {
 			var rando = Math.floor(Math.random()*vgs_arr.length);
 			vgs_final = vgs_arr[rando];
@@ -28,34 +28,34 @@ client.Dispatcher.on("MESSAGE_CREATE", e => {
 		sendMessage(e, vgs_final);
 		return;
 	}
-	if(incoming_text == '!naptime')  {
+	if(incoming_text == "!naptime")  {
 		e.message.channel.uploadFile("img/billynap.gif"); // File
 		return;
 	}
-	if(incoming_text == '!same')  {
+	if(incoming_text == "!same")  {
 		e.message.channel.uploadFile("img/same.gif"); // File
 		return;
 	}
-	if(incoming_text == '!mark' || incoming_text == '!tank') { // Mark Nickerson, our hero
+	if(incoming_text == "!mark" || incoming_text == "!tank") { // Mark Nickerson, our hero
 		e.message.channel.uploadFile("img/tank.jpg"); // File
 		return;
 	}
-	if(incoming_text == '!killyourself') { // Go back in time
-		sendMessage(e, 'Going back in time, brb.');
+	if(incoming_text == "!killyourself") { // Go back in time
+		sendMessage(e, "Going back in time, brb.");
 		client.disconnect();
 		return;
 	}
-	if(incoming_text.substring(0,1) == '!') { // Snark inc
+	if(incoming_text.substring(0,1) == "!") { // Snark inc
 		var snark = [
-			'Good morning, Paul. What will your first sequence of the day be?',
-			'I can\'t wait to entertayne you.',
-		    'Can I see a hat wobble?',
-		    'Can I see a Flarhgunnstow?',
-		    'Can you kick up the 4D3D3D3?',
-		    'add sequence: OYSTER',
-		    'Give me a print-out of Oyster smiling.',
-		    'Nude. Tayne.',
-		    'We have important work to do.'
+			"Good morning, Paul. What will your first sequence of the day be?",
+			"I can't wait to entertayne you.",
+		    "Can I see a hat wobble?",
+		    "Can I see a Flarhgunnstow?",
+		    "Can you kick up the 4D3D3D3?",
+		    "add sequence: OYSTER",
+		    "Give me a print-out of Oyster smiling.",
+		    "Nude. Tayne.",
+		    "We have important work to do."
 		];
 		var rando_snark = Math.floor(Math.random()*snark.length);
 		sendMessage(e, snark[rando_snark]);
